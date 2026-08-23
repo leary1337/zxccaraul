1378c80c2dee8adb02ae7aee1782b42c6959fe65
dn.kalabin <kalabin.danil28@mail.ru>
2026-08-23 23:19:51 +0300

    Make roster PNG action full width

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "caraul-v45";
+const CACHE = "caraul-v46";
 const ASSETS = ["/", "/index.html", "/styles.css", "/app.js", "/manifest.webmanifest", "/icon.svg"];
 
 self.addEventListener("install", (event) => {
