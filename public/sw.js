dbe15b561f188fa6e3e491442be12fa0939ebf9e
dn.kalabin <kalabin.danil28@mail.ru>
2026-08-26 15:11:13 +0300

    Stabilize mobile bottom navigation

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "caraul-v49";
+const CACHE = "caraul-v50";
 const ASSETS = ["/", "/index.html", "/styles.css", "/app.js", "/manifest.webmanifest", "/icon.svg"];
 
 self.addEventListener("install", (event) => {
